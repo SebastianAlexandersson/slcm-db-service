f271f729c26e15ad0a86c4258cfa844f220d59f8
Sebastian Alexandersson <sebastian.alexandersson@bilvision.se>
2020-10-31 19:24:05 +0100

    saving stuff

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
-});
+app.get('/', (req, res) => {
+    res.send('hello world');
+})
 
-const port = process.env.PORT || 1337;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+app.listen(PORT, () => console.log('DB Service running on port: ', + PORT));
